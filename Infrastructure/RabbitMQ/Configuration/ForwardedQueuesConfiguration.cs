d42597d3e9d7d51351bc6c521f5d154217345992
dx_sv_note <drakonfreshr@gmail.com>
2024-10-09 21:00:42 +0300

    start do rabbit configuration

---
+++ Infrastructure/RabbitMQ/Configuration/ForwardedQueuesConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.RabbitMQ.Configuration
+{
+    // это очереди привязанные к айди пользователя в которые сообщение из вне пересылаются
+    public class ForwardedQueuesConfiguration
+    {
+        public string Exchange { get; set; }
+        public string BeginningOfBindingKey { get; set; }
+    }
+}
