c8088e288e69e9f2d92ba4b88c4562841865c80b
dx_sv_note <drakonfreshr@gmail.com>
2024-10-14 13:51:22 +0300

    interim changes

---
+++ Domain/Model/UserConnection/UserConnectionBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model.UserConnection
+{
+    public delegate Task ConnectionAbortedHandler();
+    public abstract class UserConnectionBase
+    {
+        public ConnectionAbortedHandler ConnectionAbortedHandler;
+        public abstract Task SendAsync(byte[] message);
+    }
+}
